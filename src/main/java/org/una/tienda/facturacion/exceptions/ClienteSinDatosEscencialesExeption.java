d6d0c4dd74603259ccb287f1bb22bacedd15de18
Mónica <serracin.monica@gmail.com>
2020-10-18 07:38:43 -0600

    Proyecto nuevo con test corregidos

---
+++ src/main/java/org/una/tienda/facturacion/exceptions/ClienteSinDatosEscencialesExeption.java
@@ -0,0 +1,7 @@
+package org.una.tienda.facturacion.exceptions;
+
+public class ClienteSinDatosEscencialesExeption extends Exception {
+    public ClienteSinDatosEscencialesExeption(String mensaje){
+        super(mensaje);
+    }
+}
---
+++ src/main/java/org/una/tienda/facturacion/services/IFacturaService.java
@@ -0,0 +1,16 @@
+package org.una.tienda.facturacion.services;
+
+import org.una.tienda.facturacion.dto.FacturaDTO;
+import org.una.tienda.facturacion.exceptions.EvitarModificarContenidoInactivoExeption;
+
+import java.util.Optional;
+
+public interface IFacturaService {
+    public FacturaDTO create(FacturaDTO facturaDTO);
+
+    public Optional<FacturaDTO> update(FacturaDTO facturaDTO) throws EvitarModificarContenidoInactivoExeption;
+
+    public void delete(Long id);
+
+    public Optional<FacturaDTO> findById(Long id);
+}
