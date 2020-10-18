e60a5778c93d567ae470aff6a9e269e04b484c20
Mónica <serracin.monica@gmail.com>
2020-10-18 07:36:53 -0600

    Se borra

--- facturas/src/main/java/org/una/tienda/facturacion/exceptions/ClienteSinDatosEscencialesExeption.java
+++
@@ -1,7 +0,0 @@
-package org.una.tienda.facturacion.exceptions;
-
-public class ClienteSinDatosEscencialesExeption extends Exception {
-    public ClienteSinDatosEscencialesExeption(String mensaje){
-        super(mensaje);
-    }
-}
